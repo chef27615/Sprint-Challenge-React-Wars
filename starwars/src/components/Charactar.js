3aeb8f81854f85b083f44251a1faab186da06beb
Wei <chef27615@gmail.com>
2019-03-29 11:29:35 -0400

    adj style

--- starwars/src/components/Charactar.js
+++ starwars/src/components/Charactar.js
@@ -2,16 +2,17 @@ import React from 'react';
 
 const Charactar = props =>{
     
-    
+    const {name, birth_year, height, mass, eye_color, skin_color, homeworld} = props.char
     return(
         <div className="chars">
-            <ul>
-                <li>{props.char.name}</li>
-                <li>{props.char.birth_year}</li>
-                <li>{props.char.created}</li>
-                <li>{props.char.edited}</li>
-                <li>{props.char.eye_color}</li>
-                
+            <ul className="chars_detail">
+                <li>Name: {name}</li>
+                <li>Birth: {birth_year}</li>
+                <li>Height: {height}</li>
+                <li>Mass: {mass}</li>
+                <li>Eye Color {eye_color}</li>
+                <li>Skin Color {skin_color}</li>
+                <li>Home {homeworld}</li>
             </ul>
         </div>
     );
